bfe4ebbf35a04741caecebd1a79c908428b13266
sunyinglong <sunyinglong@renrencaopan.com>
2016-11-15 16:40:20 +0800

    初始化提交333

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,8 +1,13 @@
 /**
  * Created by liushaoxin on 2016/11/15.
  */
+import redis.clients.jedis.Jedis;
 public class Demo {
-    public  static  void main(String args[]){
-        System.out.println("nihao");
+    public static void main(String[] args) {
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        //check whether server is running or not
+        System.out.println("Server is running: "+jedis.ping());
     }
 }
